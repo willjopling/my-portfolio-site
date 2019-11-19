9f3f3ee8c88d9908781cfef268ced72f8c1e477f
willjopling <55174599+willjopling@users.noreply.github.com>
2019-11-19 13:39:13 +1100

    inital commit with styled components

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,92 +1,114 @@
 import React, { useState } from "react";
-import "../stylesheets/App.css";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 
 import HomeIcon from "@material-ui/icons/Home";
 import BuildIcon from "@material-ui/icons/Build";
 import MailIcon from "@material-ui/icons/Mail";
-import { makeStyles } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 import Projects from "./Projects";
 import Home from "./Home";
 import Contact from "./Contact";
 
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-
-const useStyles = makeStyles({
-  icon: {
-    color: "white"
-  }
-});
-
-export default function MenuBar(props) {
+const MenuBar = () => {
   const [selectedOption, setSelectedOption] = useState("home");
 
-  const classes = useStyles();
+  const Container = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+
+    ${css`
+      @media (max-width: 800px) {
+        flex-direction: column;
+      }
+    `}
+  `;
+
+  const MenuContainer = styled.div`
+    background: #1a1a1a;
+    left: 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    ${css`
+      @media (max-width: 800px) {
+        flex-direction: row;
+        max-height: 4em;
+      }
+    `}
+  `;
+
+  const DisplayContainer = styled.div`
+    flex: 7;
+    display: block;
+    overflow: scroll;
+  `;
 
   return (
-    <div>
-      <div id="app-container">
-        <ul id="menu-bar">
-          <li>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => {
-                setSelectedOption("home");
-              }}
-              endIcon={<HomeIcon />}
-            >
-              About
-            </Button>
-          </li>
-          <li>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => {
-                setSelectedOption("projects");
-              }}
-              endIcon={<BuildIcon />}
-            >
-              Projects
-            </Button>
-          </li>
-          <li>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => {
-                setSelectedOption("contact");
-              }}
-              endIcon={<MailIcon />}
-            >
-              Contact
-            </Button>
-          </li>
-          <li>
-            <div>
-              <IconButton href={"https://github.com/willjopling"}>
-                <GitHubIcon className={classes.icon} />
-              </IconButton>
-              <IconButton href={"https://www.linkedin.com/in/william-jopling/"}>
-                <LinkedInIcon className={classes.icon} />
-              </IconButton>
-            </div>
-          </li>
-        </ul>
-
-        <div id="display-container">{display(selectedOption)}</div>
-        <div className="page-background"></div>
-      </div>
-    </div>
+    <Container>
+      <MenuContainer>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            setSelectedOption("home");
+          }}
+          endIcon={<HomeIcon />}
+        >
+          About
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            setSelectedOption("projects");
+          }}
+          endIcon={<BuildIcon />}
+        >
+          Projects
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            setSelectedOption("contact");
+          }}
+          endIcon={<MailIcon />}
+        >
+          Contact
+        </Button>
+        <div>
+          <IconButton
+            style={{ color: "white" }}
+            href={"https://github.com/willjopling"}
+          >
+            <GitHubIcon />
+          </IconButton>
+          <IconButton
+            style={{ color: "white" }}
+            href={"https://www.linkedin.com/in/william-jopling/"}
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </div>
+      </MenuContainer>
+      <DisplayContainer>{display(selectedOption)}</DisplayContainer>
+    </Container>
   );
-}
+};
 
-const display = props => {
-  switch (props) {
+const display = selectedOption => {
+  switch (selectedOption) {
     case "projects":
       return (
         <div>
@@ -109,3 +131,5 @@ const display = props => {
       );
   }
 };
+
+export default MenuBar;
